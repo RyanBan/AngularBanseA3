7bb21f798dcd62cb00e82b129585f36f34efebf2
RyanBan <45275840+RyanBan@users.noreply.github.com>
2019-07-31 20:25:19 -0400

    Add files via upload

---
+++ assets/data/myBook.ts
@@ -0,0 +1,25 @@
+import { Book } from"../../app/book";
+export const MYLIST : Book[]=
+[{  btitle: "Momo",
+    bauthor: "Michael",
+    bgenre: "Sci-Fi",
+    byear: "2000",
+    bpic: "assets/images/momo.jpg"},
+ 
+ {  btitle: "To Kill a Mockingbird",
+    bauthor: "Harper Lee",
+    bgenre: "Fiction",
+    byear: "1960",
+    bpic: "assets/images/mock.jpg"},
+
+{   btitle: "Coraline",
+    bauthor: "Neil Gaiman",
+    bgenre: "Sci-Fi",
+    byear: "2002",
+    bpic: "assets/images/co.jpg"},
+
+{   btitle: "Harry Porter",
+    bauthor: "J. K. Rowling",
+    bgenre: "Sci-Fi",
+    byear: "2000",
+    bpic: "assets/images/hp.jpg"}]
